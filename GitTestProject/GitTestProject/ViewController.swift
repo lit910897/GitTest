19caf2a3d9738b43591c8d76cef70dec396793b3
lit910897 <75847713+lit910897@users.noreply.github.com>
2024-07-27 19:44:55 +0800

    Add files via upload
    Test

---
+++ GitTestProject/GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by 朱軒成 on 7/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        print("git")
+        print("git2")
+        print("git3")
+    }
+
+
+}
+
